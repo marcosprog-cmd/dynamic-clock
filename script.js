c6453f6afcf80f66074e43bc0e372bbb065f55e4
Marcos Daniel <130110150+marcosprog-cmd@users.noreply.github.com>
2023-05-13 12:15:37 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,19 @@
-const form = document.getElementById('form');
+const hours = document.getElementById('hours');
+const minutes = document.getElementById('minutes');
+const seconds = document.getElementById('seconds');
 
-form.addEventListener('submit', function(event) {
-    event.preventDefault();
+const clock = setInterval(function time() {
+    let dateToday = new Date();
+    let h = dateToday.getHours();
+    let min = dateToday.getMinutes();
+    let s = dateToday.getSeconds();
 
-    const weight = document.getElementById('weight').value;
-    const height = document.getElementById('height').value;
+    if (h < 10) h = '0' + h
+    if (min < 10) min = '0' + min
+    if (s < 10) s = '0' + s
 
-    const bmi = (weight / (height * height)).toFixed(2);
-    const value = document.getElementById('value');
-    let description = '';
-
-    value.classList.add('attention');
-
-    document.getElementById('infos').classList.remove('hidden');
-
-    if (bmi < 18.5) {
-        description = 'Cuidado! Seu peso esta abaixo do recomendado.';
-    } else if (bmi >= 18.5 && bmi <= 25) {
-        description = "Parabéns! Você está no peso ideal.";
-        value.classList.remove('attention');
-        value.classList.add('normal');
-    } else if (bmi > 25 && bmi <= 30) {
-        description = "Cuidado! Você está com sobrepeso.";
-    } else if (bmi > 30 && bmi <= 35) {
-        description = "Cuidado! Você está com obesidade moderada.";
-    } else if (bmi > 35 && bmi <= 40) {
-        description = "Cuidado! Você está com obesidade severa.";
-    } else {
-        description = "Cuidado! Você está com obesidade morbida.";
-    }
-
-    value.textContent = bmi.replace('.', ',');
-    document.getElementById('description').textContent = description;
-
+    hours.textContent = h;
+    minutes.textContent = min;
+    seconds.textContent = s;
 
 })
